0dc5acdfdd603b2c0837204a83d6e2fc3fa95794
tresende <thiago.gcresende@gmail.com>
2019-02-28 22:11:07 -0300

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,83 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
-      title: 'Contador de pessoas',
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/restaurant.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Pessoas: 5",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: FlatButton(
-                      child: Text(
-                        "+1",
-                        style: TextStyle(fontSize: 40, color: Colors.white),
-                      ),
-                      onPressed: () {},
+  runApp(new MaterialApp(title: 'Contador de pessoas', home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Home> {
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+      if (_people < 0) {
+        _infoText = "Mundo Invertido";
+      } else if (_people <= 10) {
+        _infoText = 'Pode Entrar';
+      } else {
+        _infoText = 'Lotado';
+      }
+    });
+  }
+
+  int _people = 0;
+  String _infoText = 'Pode Entrar';
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Pessoas: $_people",
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40, color: Colors.white),
                     ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: FlatButton(
-                      child: Text(
-                        "-1",
-                        style: TextStyle(fontSize: 40, color: Colors.white),
-                      ),
-                      onPressed: () {},
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40, color: Colors.white),
                     ),
-                  )
-                ],
-              ),
-              Text("Pode Entrar",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 30))
-            ],
-          )
-        ],
-      )));
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ),
+                )
+              ],
+            ),
+            Text(_infoText,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30))
+          ],
+        )
+      ],
+    );
+  }
 }
